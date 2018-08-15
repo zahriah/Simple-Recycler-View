2de0499d2675c03716e4ffd00a68f6709e9a3562
Zahriah Othman <zahriahothman@gmail.com>
2018-08-15 16:32:56 +0800

    Day 3 Recycler View

---
+++ app/src/main/java/my/edu/utem/simplerecyclerview/MainActivity.java
@@ -0,0 +1,53 @@
+package my.edu.utem.simplerecyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview);
+        CustomAdapter adapter = new CustomAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        TextView wordTextView;
+
+        public CustomViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.custom_row, parent, false));
+            wordTextView = itemView.findViewById(R.id.wordTextView);
+        }
+    }
+
+    public class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder> {
+        public CustomAdapter(){}
+        @NonNull
+        @Override
+        public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new CustomViewHolder(LayoutInflater.from(viewGroup.getContext()), viewGroup);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CustomViewHolder customViewHolder, int i) {
+            customViewHolder.wordTextView.setText("This is row " + i);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+    }
+}
